3c36491f2e246ccfa0205622b00f0c3868008477
borhanshuvo <borhan015@gmail.com>
2021-07-31 22:53:00 +0600

    navbar & header & plagiarism section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div style={{ backgroundColor: '#0254F9', color: 'white' }}>
             <div className="container pt-4">
                 <div className="d-flex justify-content-between align-items-center">
-                    <Image src={logo} className={styles.logo} alt="" />
+                    <Image src={logo} className={`${styles.logo}`} alt="" />
                     <div>
                         <Link href="/"><a className="mx-3">Sign Up</a></Link>
                         <Link href="/"><a>Log In</a></Link>
