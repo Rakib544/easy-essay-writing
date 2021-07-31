9b2a6a736bbb5f0bb1a2c0296e488659c06ccd59
Rakib544 <md.rakib10122003@gmail.com>
2021-07-31 14:29:32 +0600

    new branch created by me

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p className="font-bold text-primary">Hello World</p>
+      <p className="font-bold text-primary">Hello World, I am Rakib</p>
       <button className="btn btn-primary">Test</button>
     </div>
   );
