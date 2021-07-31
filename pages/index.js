aff7526b10fc88dca2d4ee03cf57633b771f3083
SAGOR <seakingmawmaw2020@gmail.com>
2021-07-31 15:16:57 +0600

    push pricing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Pricing from "../src/components/pricing/pricing";
+
 export default function Home() {
   return (
     <div>
       <p className="font-bold text-primary">Hello World, I am Rakib</p>
       <p>Development branch created and merge with Rakib</p>
       <button className="btn btn-primary">Test</button>
+      <Pricing/>
     </div>
   );
 }
---
+++ src/components/pricing/pricing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div className="container">
+            <h1>this is pricing</h1>
+        </div>
+    );
+};
+
+export default Pricing;
