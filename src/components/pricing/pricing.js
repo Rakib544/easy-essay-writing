e4ac1703b770d93ddd20ee6f4ae90d70a30dcb85
SAGOR <seakingmawmaw2020@gmail.com>
2021-07-31 15:17:54 +0600

    update

--- src/components/pricing/pricing.js
+++ src/components/pricing/pricing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Pricing = () => {
     return (
         <div className="container">
-            <h1>this is pricing</h1>
+            <h1>this is pricing update</h1>
         </div>
     );
 };
