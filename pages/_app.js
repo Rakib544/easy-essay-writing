4e383cf6cbb2b1b3e6ae380e53e2442d65b5dd40
Rakib544 <md.rakib10122003@gmail.com>
2021-07-31 19:10:46 +0600

    process section basic design complete

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,16 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        ></link>
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
---
+++ src/data/processCardData.js
@@ -0,0 +1,32 @@
+import image2 from "../../images/Grupo 359.svg";
+import image4 from "../../images/Grupo 466.svg";
+import image3 from "../../images/Image 4.svg";
+import image1 from "../../images/Image-2.png";
+
+export const processCardData = [
+  {
+    id: 1,
+    img: image1,
+    title: "Give us a description of your project.",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
+  },
+  {
+    id: 2,
+    img: image2,
+    title: "Choose your number of words/pages.",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
+  },
+  {
+    id: 3,
+    img: image3,
+    title: "Pay.",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
+  },
+  {
+    id: 4,
+    img: image4,
+    title:
+      "Have your high-quality work delivered to you by the specified date.",
+    text: "",
+  },
+];
