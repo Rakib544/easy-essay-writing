c60b741098501cd98493535900cc85fbcf943cda
AhmedKamon <ahmedkamon3@gmail.com>
2021-08-01 15:38:23 +0600

    admin

---
+++ pages/admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Admin from '../src/components/Admin/Admin';
+
+const admin = () => {
+    return (
+        <div>
+            <Admin/>
+        </div>
+    );
+};
+
+export default admin;
