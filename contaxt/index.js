6f1fde84c0959ba6daaf3ce86e92a415d73d1a5f
Rakib544 <md.rakib10122003@gmail.com>
2021-08-03 16:45:00 +0600

    new commit

--- contaxt/index.js
+++ contaxt/index.js
@@ -1,33 +1,28 @@
 import { createContext, useReducer } from "react";
-import firebase from '../firebase'
-
-
 
 //reducer
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'LOGIN':
-            return { ...state, user: action.payload }
-        case 'LOGOUT':
-            return { ...state, user: null };
-        default:
-            return state;
-
-    }
-}
-
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
 
 //initalState
 const initalState = {
-    user: null
-}
+  user: null,
+};
 //create context
-const Context = createContext({})
+const Context = createContext({});
 
 //provider consumer
 const Provider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initalState);
-    const value = { state, dispatch };
-    return <Context.Provider value={value} >{children}</Context.Provider>
-}
-export { Context, Provider }
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const value = { state, dispatch };
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+export { Context, Provider };
