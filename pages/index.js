411623cbfe142af527cd7d410e140b4b2205f94f
Rakib544 <md.rakib10122003@gmail.com>
2021-08-05 00:53:38 +0600

    home page two data dynamic value set complete

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ import Pricing from "../src/components/pricing/pricing";
 import Process from "../src/components/process/Process";
 import Question from "../src/components/Question/Question";
 
-export default function Home() {
+export default function Home({ bannerData, aboutData }) {
   return (
     <div>
       <Navbar />
-      <Header />
-      <Plagiarism />
+      <Header bannerData={bannerData} />
+      <Plagiarism aboutData={aboutData} />
       <Process />
       <div className="mx-4 mx-md-0">
         <Pricing />
@@ -20,4 +20,19 @@ export default function Home() {
       <Footer />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const bannerResponse = await fetch("http://localhost:8080/banner");
+  const bannerData = await bannerResponse.json();
+
+  const aboutResponse = await fetch("http://localhost:8080/about");
+  const aboutData = await aboutResponse.json();
+
+  return {
+    props: {
+      bannerData: bannerData[0],
+      aboutData: aboutData[0],
+    },
+  };
 }
