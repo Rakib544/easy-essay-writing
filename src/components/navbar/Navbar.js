cb743303dc6a4834ce1fbadfe083f9704dc452f3
Rakib544 <md.rakib10122003@gmail.com>
2021-08-05 23:38:27 +0600

    logout functionality done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,33 @@
+import firebase from "firebase/app";
 import "firebase/auth";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../../../images/logo.png";
+import { UserContext } from "../../../pages/_app";
+import { firebaseConfig } from "../firebaseConfig/firebase.config";
 import styles from "./navbar.module.css";
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 const Navbar = () => {
+  const [signedUser] = useContext(UserContext);
+
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setSignedUser({});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   const router = useRouter();
   const currentPath = router.pathname;
 
@@ -36,22 +57,48 @@ const Navbar = () => {
           <div className="ms-auto navbar-nav mt-3 mt-md-0">
             {router.pathname === "/" && (
               <>
-                <li className="nav-item">
-                  <Link href="/signup">
-                    <a className="mx-3 mx-md-0 text-white nav-link px-2">
-                      Sign Up
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="/signin">
-                    <a
-                      className={`${styles.navItem} mx-3 mx-md-1 text-white nav-link px-2`}
-                    >
-                      Sing In
-                    </a>
-                  </Link>
-                </li>
+                {signedUser.email ? (
+                  <>
+                    <li className="nav-item mx-md-3 my-2 my-md-0">
+                      <Link href="/admin">
+                        <a
+                          className={`${styles.navItem} mx-3 mx-md-0 text-white nav-link px-3`}
+                        >
+                          Dashboard
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link href="">
+                        <a
+                          onClick={logout}
+                          className={`${styles.navItem} mx-3 mx-md-0 text-white nav-link px-3`}
+                        >
+                          Sign Out
+                        </a>
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <Link href="/signup">
+                        <a className="mx-3 mx-md-0 text-white nav-link px-2">
+                          Sign Up
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link href="/signin">
+                        <a
+                          className={`${styles.navItem} mx-3 mx-md-1 text-white nav-link px-2`}
+                        >
+                          Sing In
+                        </a>
+                      </Link>
+                    </li>
+                  </>
+                )}
               </>
             )}
             {router.pathname === "/orderlist" && (
