17686c3ec2adbb78341a93a854eb3d5015447d1c
borhanshuvo <borhan015@gmail.com>
2021-08-05 12:11:29 +0600

    admin page process card done

--- src/components/admin/ourProcess/ourProcess.js
+++ src/components/admin/ourProcess/ourProcess.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import OurProcessCard from '../ourProcessCard/ourProcessCard';
 
 const OurProcess = () => {
+
+    const [processData, setProcessData] = useState({});
+    const { _id, title, headerDetails } = processData;
+    const [number, setNumber] = useState(0);
+    const titleValue = title;
+    const headerDetailsValue = headerDetails;
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const [processCardsData, setProcessCardsData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/process')
+            .then(res => res.json())
+            .then(data => setProcessData(data[0]));
+    }, [number]);
+
+    const onSubmit = (data) => {
+        const title = data.title || titleValue;
+        const headerDetails = data.headerDetails || headerDetailsValue;
+
+        fetch(`http://localhost:8080/process/update/${_id}`, {
+            method: 'PUT',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                title,
+                headerDetails
+            })
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result) {
+                    console.log(result)
+                    setProcessData(result);
+                    setNumber(number + 1);
+                }
+            })
+    };
+
+    useEffect(() => {
+        fetch('http://localhost:8080/processCard')
+            .then(res => res.json())
+            .then(data => setProcessCardsData(data));
+    }, [number]);
+
     return (
         <>
             <p className="bg-white py-2 px-5 d-inline-block fs-26 fw-bold fw-bold my-4 box-shadow">
@@ -12,9 +59,9 @@ const OurProcess = () => {
                     Title -
                 </p>
                 <div className="mt-4 mb-4 d-flex justify-content-between align-items-center">
-                    <p className="fs-22 fw-bold">Our Process</p>
+                    <p className="fs-22 fw-bold">{titleValue}</p>
                     <button
-                        className="btn-style"
+                        className="btn-style" data-bs-toggle="modal" data-bs-target="#processModal1"
                     >
                         Edit
                     </button>
@@ -24,52 +71,55 @@ const OurProcess = () => {
                     Header - Details
                 </p>
                 <div className="mt-4 mb-3 d-flex justify-content-between align-items-center">
-                    <p className="fs-16">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry’s standard dummy
-                        text ever since the 1500s
-                    </p>
+                    <p className="fs-16">{headerDetailsValue}</p>
                     <button
-                        className="btn-style"
+                        className="btn-style" data-bs-toggle="modal" data-bs-target="#processModal2"
                     >
                         Edit
                     </button>
                 </div>
 
                 <p className="text-primary fs-22 fw-bold mb-5">Items</p>
-
                 <div className="row mt-2">
-                    <div className="col-md-4">
-                        <p className="text-primary fs-18 fw-bold mb-2">Item - 1</p>
-                        <div className="mt-4 mb-4 d-flex justify-content-between align-items-center">
-                            <p className="border rounded fs-16 fw-bold  d-inline-block py-2 px-4">
-                                Title -
-                            </p>
-                            <button
-                                className="btn-style"
-                            >
-                                Edit
-                            </button>
-                        </div>
+                    {
+                        processCardsData.map((processCardData, index) => (
+                            <OurProcessCard processCardData={processCardData} key={processCardData._id} index={index} setNumber={setNumber} number={number} />
+                        ))
+                    }
+                </div>
 
-                        <p className="fs-18 fw-bold">
-                            Give us a description of your project.
-                        </p>
-                        <div className="mt-4 mb-4 d-flex justify-content-between align-items-center">
-                            <p className="border fs-16 fw-bold rounded d-inline-block py-2 px-4">
-                                Content
-                            </p>
-                            <button
-                                className="btn-style"
-                            >
-                                Edit
-                            </button>
-                        </div>
+            </div>
 
-                        <p className="fs-16">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum
-                        </p>
+            <div class="modal fade" id="processModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <textarea rows="5" cols="5" defaultValue={titleValue} {...register("title")} name='title' id='title' class="form-control mb-2"></textarea>
+                                <input type="submit" className="btn btn-primary" value="Save Changes" data-bs-dismiss="modal" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="processModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Header - Details</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <textarea rows="5" cols="5" defaultValue={headerDetailsValue} {...register("headerDetails")} name='headerDetails' id='headerDetails' class="form-control mb-2"></textarea>
+                                <input type="submit" className="btn btn-primary" value="Save Changes" data-bs-dismiss="modal" />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/admin/ourProcessCard/ourProcessCard.js
+++ src/components/admin/ourProcessCard/ourProcessCard.js
@@ -1,38 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-const BannerInfo = () => {
+const OurProcessCard = ({ processCardData, index, setNumber, number }) => {
 
-    const [bannerData, setBannerDate] = useState({});
-    const { _id, title, headerDetails } = bannerData;
-    const [number, setNumber] = useState(0);
-    const titleValue = title;
-    const headerDetailsValue = headerDetails;
-
+    const { _id, title, content } = processCardData;
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    useEffect(() => {
-        fetch('http://localhost:8080/banner')
-            .then(res => res.json())
-            .then(data => setBannerDate(data[0]));
-    }, [number]);
-
     const onSubmit = (data) => {
-        const title = data.title || titleValue;
-        const headerDetails = data.headerDetails || headerDetailsValue;
 
-        fetch(`http://localhost:8080/banner/update/${_id}`, {
+        const title = data.title;
+        const content = data.content;
+
+        fetch(`http://localhost:8080/processCard/update/${_id}`, {
             method: 'PUT',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 title,
-                headerDetails
+                content
             })
         })
             .then(res => res.json())
             .then(result => {
                 if(result) {
-                    setBannerDate(result);
+                    console.log(result)
                     setNumber(number + 1);
                 }
             })
@@ -40,37 +30,34 @@ const BannerInfo = () => {
 
     return (
         <>
-            <p className="bg-white py-2 px-5 d-inline-block fs-26 fw-bold my-4 box-shadow">
-                Banner Info
-            </p>
-
-            <div className="bg-white py-4 px-5 my-4 box-shadow">
-                <p className="border rounded d-inline-block fw-bold fs-22 py-2 px-4">
-                    Title -
-                </p>
+            <div className="col-md-4 pb-3">
+                <p className="text-primary fs-18 fw-bold mb-2">Item - {index + 1}</p>
                 <div className="mt-4 mb-4 d-flex justify-content-between align-items-center">
-                    <p className="fs-22 fw-bold">{titleValue}</p>
+                    <p className="border rounded fs-16 fw-bold  d-inline-block py-2 px-4">
+                        Title -
+                    </p>
                     <button
-                        className="btn-style" data-bs-toggle="modal" data-bs-target="#bannerModal1"
+                        className="btn-style" data-bs-toggle="modal" data-bs-target={`#processCardModal${index + 1}`}
                     >
                         Edit
                     </button>
                 </div>
+                <p className="fs-18 fw-bold">{title}</p>
 
-                <p className="border rounded d-inline-block fs-22 fw-bold py-2 px-4">
-                    Header - Details
-                </p>
-                <div className="mt-4 mb-3 d-flex justify-content-between align-items-center">
-                    <p className="fs-16">{headerDetailsValue}</p>
+                <div className="mt-4 mb-4 d-flex justify-content-between align-items-center">
+                    <p className="border fs-16 fw-bold rounded d-inline-block py-2 px-4">
+                        Content
+                    </p>
                     <button
-                        className="btn-style" data-bs-toggle="modal" data-bs-target="#bannerModal2"
+                        className="btn-style" data-bs-toggle="modal" data-bs-target={`#processModalCard${index + 1}`}
                     >
                         Edit
                     </button>
                 </div>
+                <p className="fs-16">{content}</p>
             </div>
 
-            <div class="modal fade" id="bannerModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id={`processCardModal${index + 1}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -79,7 +66,7 @@ const BannerInfo = () => {
                         </div>
                         <div class="modal-body">
                             <form onSubmit={handleSubmit(onSubmit)}>
-                                <textarea rows="5" cols="5" defaultValue={titleValue} {...register("title")} name='title' id='title' class="form-control mb-2"></textarea>
+                                <textarea rows="5" cols="5" defaultValue={title} {...register("title")} name='title' id='title' class="form-control mb-2"></textarea>
                                 <input type="submit" className="btn btn-primary" value="Save Changes" data-bs-dismiss="modal" />
                             </form>
                         </div>
@@ -87,16 +74,17 @@ const BannerInfo = () => {
                 </div>
             </div>
 
-            <div class="modal fade" id="bannerModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id={`processModalCard${index + 1}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Header - Details</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Content</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <form onSubmit={handleSubmit(onSubmit)}>
-                                <textarea rows="5" cols="5" defaultValue={headerDetailsValue} {...register("headerDetails")} name='headerDetails' id='headerDetails' class="form-control mb-2"></textarea>
+                                <textarea rows="5" cols="5" defaultValue={content} {...register("content")} name='content' id='content' class="form-control mb-2"></textarea>
+                                {/* <input type="hidden" name="_id" defaultValue={processCardData._id} {...register("_id")} id='_id' /> */}
                                 <input type="submit" className="btn btn-primary" value="Save Changes" data-bs-dismiss="modal" />
                             </form>
                         </div>
@@ -107,4 +95,4 @@ const BannerInfo = () => {
     );
 };
 
-export default BannerInfo;
+export default OurProcessCard;
