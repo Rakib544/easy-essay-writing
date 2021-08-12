7e8d264dbc7c0de6af081f3eee6891c707910116
Rakib544 <md.rakib10122003@gmail.com>
2021-08-13 01:26:05 +0600

    new commit

--- src/components/TotalOrderCard/totalOrderCard.js
+++ src/components/TotalOrderCard/totalOrderCard.js
@@ -3,18 +3,20 @@ import { useContext } from "react";
 import { UserContext } from "../../../pages/_app";
 import styles from "./totalOrderCard.module.css";
 
-const TotalOrderCard = () => {
+const TotalOrderCard = ({ userOrders }) => {
   const [signedUser] = useContext(UserContext);
   return (
     <div className="p-4 bg-white">
       <div className="d-flex">
-        <Image
-          src={signedUser.photoURL}
-          alt="profile"
-          height="50"
-          width="50"
-          className="rounded-3"
-        />
+        {signedUser.photoURL && (
+          <Image
+            src={signedUser.photoURL}
+            alt="profile"
+            height="50"
+            width="50"
+            className="rounded-3"
+          />
+        )}
         <div className="ms-3">
           <p className={`${styles.name} fs-28 fw-bold`}>{signedUser.name}</p>
           <small className={`${styles.email} fs-16`}>{signedUser.email}</small>
@@ -22,7 +24,9 @@ const TotalOrderCard = () => {
       </div>
       <div className={`p-4 ${styles.border} my-4`}>
         <small className={styles.totalOrder}>Total Orders</small>
-        <p className={`${styles.totalItemsText} fs-48 fw-bold`}>5 - Items</p>
+        <p className={`${styles.totalItemsText} fs-48 fw-bold`}>
+          {userOrders.length} - Items
+        </p>
         <p className={`${styles.lastOrderDateText} fs-19`}>
           Last Order - 23 Oct, 2021
         </p>
