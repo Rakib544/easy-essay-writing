024284443ede7865293bbee448c618dc52ce0ab7
borhanshuvo <borhan015@gmail.com>
2021-08-13 17:29:49 +0600

    changes

--- pages/paymentMethod.js
+++ pages/paymentMethod.js
@@ -6,7 +6,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import btcImage from "../images/btc.svg";
-import logoImage from "../images/logo.png";
+import logoImage from "../images/stripe.png";
 import withAuth from "../src/components/privateRoute";
 
 const stripePromise = loadStripe(process.env.stripe_public_key);
@@ -46,38 +46,43 @@ const PaymentMethod = () => {
         draggable
         pauseOnHover
       />
-      <div className="row vh-100 w-100">
-        <div className="col-md-6 mt-5 mt-md-0 pb-5 bg-white d-flex justify-content-center align-items-center">
-          <div className="text-center">
-            <div className="rounded p-5" style={{ background: "#556cd6" }}>
-              <Image src={logoImage} />
-            </div>
-            <p className="fs-44 mt-4">PAY WITH STRIPE</p>
-            <button
-              role="link"
-              onClick={createCheckoutSession}
-              className="stripe-btn d-block"
-            >
-              Stripe
-            </button>
-          </div>
-        </div>
-        <div
-          className="col-md-6 d-flex justify-content-center align-items-center py-5 py-md-0 "
-          style={{ background: "#556cd6" }}
-        >
-          <div className="text-center">
-            <Image src={btcImage} className="p-5 p-md-0" />
-            <p className="text-white fs-44 pb-3 py-mb-0">PAY WITH BTC</p>
-
-            <CopyToClipboard
-              text="3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG"
-              onCopy={() => toast.success("Copied")}
-            >
-              <p className="bg-white px-1 px-md-5 py-1 py-md-3 rounded fs-22 fs-sm-22 cursor-pointer">
-                3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG
+      <div>
+        <div className="row vh-100 w-100">
+          <div className="col-md-6 mt-5 mt-md-0 pb-5 bg-white d-flex justify-content-center align-items-center">
+            <div className="text-center">
+              <Image src={logoImage} className="w-75" />
+              <p className="fs-44 fw-bold">
+                <i>PAY WITH STRIPE</i>
               </p>
-            </CopyToClipboard>
+              <div className="px-md-5 mx-md-5 ps-3 ps-md-0">
+                <button
+                  role="link"
+                  onClick={createCheckoutSession}
+                  className="stripe-btn d-block px-md-5"
+                >
+                  Continue with Stripe
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-md-6 d-flex justify-content-center align-items-center py-5 py-md-0 "
+            style={{ background: "#556cd6" }}
+          >
+            <div className="text-center mt-md-4">
+              <Image src={btcImage} className="p-5 p-md-0" />
+              <p className="text-white fs-44 py-mb-0 mt-md-5 fw-bold">
+                <i>PAY WITH BTC</i>
+              </p>
+              <CopyToClipboard
+                text="3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG"
+                onCopy={() => toast.success("Copied")}
+              >
+                <p className="bg-white px-1 px-md-5 py-1 py-md-3 rounded fs-22 fs-sm-22 cursor-pointer">
+                  3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG
+                </p>
+              </CopyToClipboard>
+            </div>
           </div>
         </div>
       </div>
