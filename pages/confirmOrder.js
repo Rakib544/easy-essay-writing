84608586aeba1434f8e7d70f07289140f245e3b4
Rakib544 <md.rakib10122003@gmail.com>
2021-08-13 17:30:45 +0600

    new commit

--- pages/confirmOrder.js
+++ pages/confirmOrder.js
@@ -1,11 +1,13 @@
 import jwt_encode from "jwt-encode";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useContext } from "react";
 import { FiCheckCircle } from "react-icons/fi";
 import { useEffect, useState } from "react/cjs/react.development";
+import withAuth from "../src/components/privateRoute";
 import { UserContext } from "./_app";
 
-const confirmOrder = () => {
+const ConfirmOrder = () => {
   const [singedUser] = useContext(UserContext);
 
   //updating user info in localStorage
@@ -33,11 +35,14 @@ const confirmOrder = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        fetch(`https://essay-essay-writing.herokuapp.com/create/update/${singedUser._id}`, {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify(),
-        })
+        fetch(
+          `https://essay-essay-writing.herokuapp.com/create/update/${singedUser._id}`,
+          {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(),
+          }
+        )
           .then((res) => res.json())
           .then((result) => {
             const token = jwt_encode(userInfo, "secret");
@@ -49,17 +54,24 @@ const confirmOrder = () => {
       });
   };
   return (
-    <div className="m-auto p-auto mt-5 pt-5">
-      <div className="text-center mt-5 pt-5">
-        <h1>Thank You</h1>
-        <p>Press Confirm button To place Order</p>
-        <button className="btn btn-success" onClick={handleOrder}>
-          {" "}
-          Confirm Order <FiCheckCircle />
-        </button>
+    <>
+      <Head>
+        <title>Easy Essay Writing | Confirm Order</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="m-auto p-auto mt-5 pt-5">
+        <div className="text-center mt-5 pt-5">
+          <h1>Thank You</h1>
+          <p>Your Payment has been successfully placed</p>
+          <p>Please, Press Confirm button To place the Order</p>
+          <button className="btn btn-success" onClick={handleOrder}>
+            {" "}
+            Confirm Order <FiCheckCircle />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default confirmOrder;
+export default withAuth(ConfirmOrder);
