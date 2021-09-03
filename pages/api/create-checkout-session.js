0330a18236f78e1bcd4b5358e19ea0838dea4762
borhanshuvo <borhan015@gmail.com>
2021-09-03 19:49:25 +0600

    changes

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,4 +1,6 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51JUhCFEEVYbKXLZGIHx8YuiTegXF7X2uINXeBJe83E4YPEUIirrSH68DznnPHCbq74rhY8ZJYE3ZAzB8l1jhyd7m00b24In4Ff"
+);
 
 export default async (req, res) => {
   try {
--- pages/paymentMethod.js
+++ pages/paymentMethod.js
@@ -1,28 +1,30 @@
-import { loadStripe } from '@stripe/stripe-js';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import btcImage from '../images/btc.svg';
-import logoImage from '../images/stripe.png';
-import withAuth from '../src/components/privateRoute';
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import btcImage from "../images/btc.svg";
+import logoImage from "../images/stripe.png";
+import withAuth from "../src/components/privateRoute";
 
-const stripePromise = loadStripe(process.env.stripe_public_key);
-const axios = require('axios');
+const stripePromise = loadStripe(
+  "pk_test_51JUhCFEEVYbKXLZGH7jJIqVNwCrVG2etVjDrXn8cjIJtzMDsxMe9hd4TW91fsCs788S3NbAbg5fQcvY7OUCea7NP00tMhhwuB9"
+);
+const axios = require("axios");
 
 const PaymentMethod = () => {
   const router = useRouter();
   const [orderInfo, setOrderInfo] = useState({});
 
   useEffect(() => {
-    setOrderInfo(JSON.parse(window.localStorage.getItem('orderInfos')));
+    setOrderInfo(JSON.parse(window.localStorage.getItem("orderInfos")));
   }, []);
 
   const createCheckoutSession = async () => {
     const stripe = await stripePromise;
-    const checkoutSession = await axios.post('./api/create-checkout-session', {
+    const checkoutSession = await axios.post("./api/create-checkout-session", {
       orderAmount: orderInfo.orderAmount,
       deliveryTime: orderInfo.deliveryTime,
       customerEmail: orderInfo.customerEmail,
@@ -67,7 +69,7 @@ const PaymentMethod = () => {
           </div>
           <div
             className="col-md-6 d-flex justify-content-center align-items-center py-5 py-md-0 "
-            style={{ background: '#556cd6' }}
+            style={{ background: "#556cd6" }}
           >
             <div className="text-center mt-md-4">
               <Image src={btcImage} className="p-5 p-md-0" />
@@ -76,7 +78,7 @@ const PaymentMethod = () => {
               </p>
               <CopyToClipboard
                 text="3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG"
-                onCopy={() => toast.success('Copied')}
+                onCopy={() => toast.success("Copied")}
               >
                 <p className="bg-white px-1 px-md-5 py-1 py-md-3 rounded fs-22 fs-sm-22 cursor-pointer">
                   3EXDYQqE17N7NE4VRYJoqBmSQ5UFc9vzTG
@@ -90,4 +92,4 @@ const PaymentMethod = () => {
   );
 };
 
-export default PaymentMethod;
+export default withAuth(PaymentMethod);
